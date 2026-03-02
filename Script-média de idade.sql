dbfd9ed273a2137d0dd3155fffd0ccd0976bc962
FilipaPereira-IA <filipa.apereira@outlook.com>
2026-03-02 11:12:46 +0000

    Delete Script-média de idade.sql

--- Script-média de idade.sql
+++
@@ -1,7 +0,0 @@
--- Média de Idade por Sentimento Predito pela IA
-SELECT 
-    sentimento_predito, 
-    COUNT(*) AS total_clientes,
-    ROUND(AVG(Age), 2) AS idade_media
-FROM perfil_clientes
-GROUP BY sentimento_predito;
